1f9f7b7169a08bdc688f2e12624c93cf974b59cb
Hudson Cunha <hudson_uzumaki2@hotmail.com>
2020-07-31 12:13:13 -0300

    atualização1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <BannerMain
       videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+      videoDescription={"Falamos de diversas coisas sobre o mundo nerd,desde desenhos,músicas,filmes,jogos e até mesmo curiosidades.Venha se divertir!!!!!"}
     />
 
     <Carousel
